215893ea44115e413dbadad5faceabdd1d1c006a
Amin Shazrin <aminshazrin@yahoo.com>
2018-04-01 00:20:50 +0800

    Dah belajar sampai sizeOf

--- C Plus Plus Tutorial/main.cpp
+++ C Plus Plus Tutorial/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <string> // to include string
 using namespace std; // to shorten the code that usually use std::cout to do COUT ONLY. Wtf right ?
-int main()
+// int main basic running the command prompt for C++
+int main(int argc , const char* argv[])
 {
 	// Cout style
 	cout << "Hello World ! \n" << endl; // std stands for standard
@@ -20,16 +22,43 @@ int main()
 	Use data type to declare which type you want , THEN you can declare any variable_names. In Javascript
 	you just have simply use var right ? Its is so afk when C++ have so many data types to use. Lol
 	*/
-	int i= 10;
-	i = i + 10;
-	bool b = false;
-	float x = 4.56432;
-	double a = 3.425234;
-	char c = '5';
-	cout << "The output is for i + 10 = " << i << "\nDoes it true or false ?" << b << endl; // must use << rather than javascript use + sign only. The fuck
-	cout << "For float = " << x << endl;
-	cout << "Double variable a = " << a << endl;
-	cout << "Character = " << c << endl;
+
+	// Console Output and other expressions
+	//int i= 10;
+	//i = i + 10;
+	//bool b = false;
+	//float x = 4.56432;
+	//double a = 3.425234;
+	//char c = '5';
+	//cout << "The output is for i + 10 = " << i << "\nDoes it true or false ? " << b << endl; // must use << rather than javascript use + sign only. The fuck
+	//cout << "For float = " << x << endl;
+	//cout << "Double variable a = " << a << endl;
+	//cout << "Character = " << c << endl;
+
+
+	// Console input
+	/*int i;
+	cout << "Please enter a number : ";
+	cin >> i;
+	cout << "Your number :\n";
+	cout << i << endl;*/
+
+	// Like javascript if else ES6
+	/*int result = 4 == 5 ? 1 : 2;
+	cout << result << endl;*/
+
+	// Comma Operator
+	/*int a;
+	int b = 7;
+	a = (b = 3, b + 2);
+	cout << "\n" << a << "\n" << endl;*/
+
+	// Type Casting Operator
+	// Notice it has int(i) ? 
+	// You can pass float into NEW DATA TYPE using type casting operator
+	float i = 5.67;
+	cout << int(i) << endl;
+
 	system("pause"); // to pause the CMD without auto exiting
-	return 0;
+	//return 0;
 }
